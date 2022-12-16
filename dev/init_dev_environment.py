b0c11e9df7d3e7f21aeb06217ae87aa8636ecd24
tcm5343 <gunstuff1@tutanota.com>
2022-12-15 20:10:23 -0600

    add code to create wallet

--- dev/init_dev_environment.py
+++ dev/init_dev_environment.py
@@ -1,28 +1,49 @@
 import json
-import base64
 import uuid
+import sys
+import logging
 
 import requests
 
-wallet_name = 'test_wallet'
-body = json.dumps({
-    'jsonrpc': '1.0',
-    'id': uuid.uuid4().urn.split(':')[-1],
-    'method': 'getbalance',
-    # 'params': ['arg1', 'arg2']
-})
-encoded_auth = base64.b64encode('username:password'.encode('UTF-8'))
-
-resp = requests.post(
-    f'http://127.0.0.1:18443/wallet/{wallet_name}',
-    data=body,
-    auth=('username', 'password')
+logging.basicConfig(
+    stream=sys.stdout,
+    format="%(levelname)s - %(asctime)s - %(message)s",
+    level=logging.INFO,
 )
+log = logging.getLogger(__name__)
+wallet_name = 'test_wallet'
+
+
+def __call_bitcoin_core(method: str, args: list):
+    body = json.dumps({
+        'jsonrpc': '1.0',
+        'id': uuid.uuid4().urn.split(':')[-1],
+        'method': method,
+        'params': args
+    })
+    resp = requests.post(
+        f'http://127.0.0.1:18443/wallet/{wallet_name}',
+        data=body,
+        auth=('username', 'password')
+    )
+    return resp.json()
+
+
+def create_btc_core_wallet():
+    create_wallet_resp = __call_bitcoin_core('createwallet', ['lightning_ledger_dev_wallet'])
+    if create_wallet_resp.get('error'):
+        if create_wallet_resp['error']['code'] == -4:
+            log.info('wallet already exists')
+        else:
+            raise Exception(f'Error creating wallet: {create_wallet_resp}')
+    else:
+        log.info('new wallet created')
+
 
 # crete wallet
+create_btc_core_wallet()
 # generate an address
 # mine 100 blocks and send to the generated address
 
 # set up alice wallet, load with funds, peer w/ bob, open channel
 
-print(resp.json())
